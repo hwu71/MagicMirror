649e39bb5a18b2421d977193092bdb9c7f400f90
joezie <joezie@163.com>
2020-04-24 00:25:14 -0400

    XL: ran a process to execute import script on raspberry pi

--- modules/third_party/FaceRecognition/PhotoCollectionRequestHandler.java
+++ modules/third_party/FaceRecognition/PhotoCollectionRequestHandler.java
@@ -2,8 +2,15 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.lang.ProcessBuilder;
 
 public class PhotoCollectionRequestHandler implements Runnable {
+	
+	// shell script names
+	private static final String importScript = "my_import.sh";
+	private static final String registerScript = "my_register.sh";
+	private static final String encodeScript = "my_encode.sh";
+	private static final String recognitionScript = "my_recog_run.sh";
 
     // a flag indicating if the photo collection is triggered successfully
     private boolean triggerSuccessFlag;
@@ -68,7 +75,45 @@ public class PhotoCollectionRequestHandler implements Runnable {
         // TODO: trigger photo collection shell script and collect photos for this user
         //       and don't forget to update triggerSuccessFlag
         //       only trigger if username is not null
-		triggerSuccessFlag = true;
+		if (username != null) {
+			
+			final ProcessBuilder builder = new ProcessBuilder();
+			
+			// import cv environment
+			System.out.println("Running " + importScript + "...");
+			builder.command(".", importScript);
+			try {
+				
+				final Process process = builder.start();
+				final int exitVal = process.waitFor();
+				
+				if (exitVal == 0)
+					triggerSuccessFlag = true;
+				else
+					triggerSuccessFlag = false;
+				
+			} catch (IOException | InterruptedException e) {
+				
+				e.printStackTrace();
+				triggerSuccessFlag = false;
+
+			}
+			
+			// collect face photos
+			
+			
+			
+			
+			// train dataset
+			
+		} else {
+			
+			triggerSuccessFlag = false;
+			
+		}
+		
+		
+		
 
         // reply a confirmation message to client
         try {
