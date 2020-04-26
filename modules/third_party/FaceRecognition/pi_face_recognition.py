5885a6ff6d98567b932b26975de1bdbcafb23f5c
joezie <joezie@163.com>
2020-04-26 01:55:04 -0400

    XL: added debug output

--- modules/third_party/FaceRecognition/pi_face_recognition.py
+++ modules/third_party/FaceRecognition/pi_face_recognition.py
@@ -160,6 +160,8 @@ while totalsent < len(most_common):
 	
 # receive a confirmation message from server
 success_status = connection_to_server.recv(1)
+print("success status:") #####################
+print(success_status) ###################
 if success_status == 1:
 	os.system("bash /home/pi/MagicMirror/modules/third_party/FaceRecognition/my_refresh.sh")
 	print("Refreshed smart mirror!")
